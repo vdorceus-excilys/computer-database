21777933e99c0095de7e4ea3f3540a6ea058e456
vdorceus-excilys <slavole@excilys.com>
2019-05-17 11:02:53 +0200

    adding support for resource resolver

--- src/main/java/com/excilys/training/config/ConfigurationSpringMVC.java
+++ src/main/java/com/excilys/training/config/ConfigurationSpringMVC.java
@@ -5,21 +5,26 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.excilys.training")
-public class ConfigurationSpringMVC {
+public class ConfigurationSpringMVC implements WebMvcConfigurer{
 	
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("/WEB-INF/views");
+		viewResolver.setPrefix("/WEB-INF/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/asset/**").addResourceLocations("/asset/");
+	}
 }
