5484b03205a2a796a8c4cde9593f01a96ca8adbd
vdorceus-excilys <slavole@excilys.com>
2019-05-17 10:41:02 +0200

    adding global web configuration support

--- src/main/java/com/excilys/training/config/GlobalConfiguration.java
+++ src/main/java/com/excilys/training/config/GlobalConfiguration.java
@@ -1,18 +1,23 @@
 package com.excilys.training.config;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class GlobalConfiguration implements WebApplicationInitializer{
 	
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-		rootContext.register(AppConfig.class);
-		servletContext.addListener(new ContextLoaderListener(rootContext));
+		rootContext.register(ConfigurationSpringMVC.class);
+		rootContext.setServletContext(servletContext);
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispacher", new DispatcherServlet(rootContext));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+		
 	}
 
 }
